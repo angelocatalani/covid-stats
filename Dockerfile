6b9c1f1f9ea89ee81744d5f8951613fc6a32db60
angelobs <anc@bendingspoons.com>
2020-09-15 23:15:53 +0200

    feat: add Grafana and Prometheus with dashboards inside config files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.8.5-buster
+RUN apt-get update
+ENV PYTHONPATH="/app" \
+  PYTHONUNBUFFERED=1 \
+  PIP_NO_CACHE_DIR=on \
+  PIP_DISABLE_PIP_VERSION_CHECK=on \
+  POETRY_VERSION=1.0.10 \
+  POETRY_VIRTUALENVS_CREATE=false \
+  PATH="${PATH}:/root/.poetry/bin"
+
+WORKDIR /app
+COPY pyproject.toml poetry.lock ./
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+RUN poetry install --no-dev
+COPY covid_stats ./covid_stats
+EXPOSE 8000/tcp
+ENTRYPOINT ["python","./covid_stats/main.py"]
--- covid_stats/main.py
+++ covid_stats/main.py
@@ -1,3 +1,23 @@
-if __name__ == "__main__":
-    # todo
-    print("COVID-19 stats")
+from prometheus_client import start_http_server, Counter
+import random
+import time
+
+c  = Counter(
+    namespace="app",
+    subsystem="stats",
+    name="covid_stats",
+    documentation="doc",
+    labelnames=("ctr",),
+)
+
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        c.labels(ctr="sample").inc()
+        time.sleep(1)
