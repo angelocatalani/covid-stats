3468c63ae2c936f2c13feb6539cd232d4fda6bc4
angelobs <anc@bendingspoons.com>
2020-09-16 17:46:12 +0200

    feat(grafana): update grafana version

--- covid_stats/main.py
+++ covid_stats/main.py
@@ -1,8 +1,8 @@
 from prometheus_client import start_http_server, Counter
-import random
 import time
 
-c  = Counter(
+# app_stats_covid_stats_total{ctr="sample",instance="app:8000",job="app"}
+sample_counter = Counter(
     namespace="app",
     subsystem="stats",
     name="covid_stats",
@@ -10,14 +10,9 @@ c  = Counter(
     labelnames=("ctr",),
 )
 
-def process_request(t):
-    """A dummy function that takes some time."""
-    time.sleep(t)
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
     start_http_server(8000)
-    # Generate some requests.
     while True:
-        c.labels(ctr="sample").inc()
+        sample_counter.labels(ctr="sample").inc()
         time.sleep(1)
